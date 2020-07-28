410e4878d97ec36b20d0f2115cc3282db03a95dc
webby12333 <webby12333@yahoo.com>
2020-07-27 20:18:29 -0500

    Finished

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,20 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        Iterable<Job> jobs;
+        if (searchType.toLowerCase().equals("all") || searchType.equals("")){
+            jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
 
+        }
+       model.addAttribute("columns", columnChoices);
+       model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 }
